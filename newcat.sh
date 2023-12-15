4ea47d9f79c1ebf01776b6b77c9fa8b8f2c3a643
Mohammed Abdulah Quraishi <mdabdulahquraish@gmail.com>
2023-12-15 12:10:58 +0530

    reloading catalogue in git

--- newcat.sh
+++ newcat.sh
@@ -37,3 +37,50 @@ VALIDATE $? "Enabling of version 18 of Nodejs"
 
 dnf install nodejs -y &>> $LOGFILE
 VALIDATE $? "Installing of version 18 of Nodejs"
+
+id roboshop
+if [ $? -ne 0 ]
+then 
+    useradd roboshop &>> $LOGFILE
+    VALIDATE $? "roboshop user creation"
+else
+    echo -e "roboshop user already exist $Y SKIPPING $N"
+fi
+
+mkdir -p /app &>> $LOGFILE
+VALIDATE $? "creating app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOGFILE
+VALIDATE $? "Downloading catalogue application"
+
+cd /app 
+
+unzip /tmp/catalogue.zip &>> $LOGFILE
+VALIDATE $? "Unzipping catalogue"
+
+cd /app
+
+npm install &>> $LOGFILE
+VALIDATE $? "Installing dependencies"
+
+cp /home/centos/Roboshop/catalogue.service  /etc/systemd/system/catalogue.service &>> $LOGFILE
+VALIDATE $? "copying catalogue service file"
+
+systemctl daemon-reload &>> $LOGFILE
+VALIDATE $? "catalogue daemon reload"
+
+systemctl enable catalogue &>> $LOGFILE
+VALIDATE $? "Enable catalogue"
+
+systemctl start catalogue &>> $LOGFILE
+VALIDATE $? "Starting catalogue"
+
+cp /home/centos/Roboshop/mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "copying mongodb repo"
+
+dnf install mongodb-org-shell -y &>> $LOGFILE
+VALIDATE $? "Installing mongodb client"
+
+mongo --host mongodb.littlesimba.online </app/schema/catalogue.js &>> $LOGFILE
+
+VALIDATE $? "Loading catalouge data into MongoDB" 
