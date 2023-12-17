97838cd9eb13fdd0e17f4d56aa6ed0aa1a98428b
Mohammed Abdulah Quraishi <mdabdulahquraish@gmail.com>
2023-12-17 16:00:15 +0530

    pushing all components of roboshop

--- redis.sh
+++ redis.sh
@@ -22,10 +22,10 @@ fi
 VALIDATE() {
     if [ $1 -ne 0 ]
     then
-    echo -e "$2 .... $R FAILED" &>> $LOGFILE
+    echo -e "$2 .... $R FAILED $N" 
     exit 1
     else
-    echo -e "$2 .... $G SUCCESS" &>> $LOGFILE
+    echo -e "$2 .... $G SUCCESS $N"
     fi
 }
 
