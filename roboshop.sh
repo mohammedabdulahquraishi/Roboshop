3e0b282411590c8958dd346a0e5fe36745f6cc10
Mohammed Abdulah Quraishi <mdabdulahquraish@gmail.com>
2023-12-20 17:26:29 +0530

    Automation of creating instances

---
+++ roboshop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+AMI_ID=ami-03265a0778a880afb
+SG_ID=sg-07062d9c12192a838
+INSTANCES=("MONGODB" "CART" "CATALOGUE" "MYSQL" "PAYMENT" "RABBITMQ" "REDIS" "SHIPPING" "USER" "WEB")
+
+for i in  "${INSTANCES[@]}"
+do
+    echo "Instance is: $i"
+    if [ $i == "MONGODB" ] || [ $i == "MYSQL" ] || [ $i == "SHIPPING" ]
+    then 
+        INSTANCE_TYPE="t3.small"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+
+     aws ec2 run-instances --image-id ami-03265a0778a880afb 
+    --instance-type $INSTANCE_TYPE --security-group-ids sg-x07062d9c12192a838
+done     
