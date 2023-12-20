a3d2a0caff19a9781e5a9b80bd76f94cd750cbd7
Mohammed Abdulah Quraishi <mdabdulahquraish@gmail.com>
2023-12-20 17:46:06 +0530

    Automation of creating instances

--- roboshop.sh
+++ roboshop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 AMI_ID=ami-03265a0778a880afb
 SG_ID=sg-07062d9c12192a838
-INSTANCES=("MONGODB" "CART" "CATALOGUE" "MYSQL" "PAYMENT" "RABBITMQ" "REDIS" "SHIPPING" "USER" "WEB")
+INSTANCES=("MONGODB") # "CART" "CATALOGUE" "MYSQL" "PAYMENT" "RABBITMQ" "REDIS" "SHIPPING" "USER" "WEB")
 
 for i in  "${INSTANCES[@]}"
 do
@@ -13,5 +13,6 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-     aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-x07062d9c12192a838
+     aws ec2 run-instances --image-id ami-03265a0778a880afb 
+    --instance-type $INSTANCE_TYPE --security-group-ids sg-x07062d9c12192a838
 done     
