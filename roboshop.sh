a1c627d665c2c699f98553d19d5fe46d291f626a
Mohammed Abdulah Quraishi <mdabdulahquraish@gmail.com>
2023-12-20 18:45:14 +0530

    Automation of creating instances

--- roboshop.sh
+++ roboshop.sh
@@ -13,7 +13,7 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-07062d9c12192a838
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-07062d9c12192a838 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
 
-
-done     
+    echo "$i: $IP_ADDRESS"
+done   
