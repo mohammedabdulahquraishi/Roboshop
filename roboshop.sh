de25bbbefe77a30f2ded6b7941c17e1ff85069af
Mohammed Abdulah Quraishi <mdabdulahquraish@gmail.com>
2023-12-21 10:19:23 +0530

    creating instances automatically

--- roboshop.sh
+++ roboshop.sh
@@ -2,6 +2,16 @@
 
 AMI=ami-03265a0778a880afb
 SG_ID=sg-07062d9c12192a838
+ZONE_ID=Z02773932U4NB4GX343HA
+INSTANCE=("mongodb" "user" "web" "cart")
 
-
-aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t2.micro --security-group-ids sg-07062d9c12192a838
+for i in "{$INSTANCE[@]}"
+do
+    if {$INSTANCE[$i] == "mongodb"}
+    then
+    INSTANCE_TYPE="t3.small"
+    else
+    INSTANCE_TYPE="t2.micro"
+    fi
+aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-07062d9c12192a838
+done
