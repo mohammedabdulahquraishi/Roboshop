fde1ce2668ec5d3c15a93eea29381c2ddd25d859
Mohammed Abdulah Quraishi <mdabdulahquraish@gmail.com>
2023-12-21 10:36:58 +0530

    creating instances automatically

--- roboshop.sh
+++ roboshop.sh
@@ -3,15 +3,35 @@
 AMI=ami-03265a0778a880afb
 SG_ID=sg-07062d9c12192a838
 ZONE_ID=Z02773932U4NB4GX343HA
-INSTANCE=("mongodb" "user" "web" "cart")
+INSTANCE=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+DOMAIN_NAME="littlesimba.online"
 
 for i in "{$INSTANCE[@]}"
 do
-    if {$INSTANCE[$i] == "mongodb"}
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
     INSTANCE_TYPE="t3.small"
     else
     INSTANCE_TYPE="t2.micro"
     fi
-aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-07062d9c12192a838
+IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-07062d9c12192a838b3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "CREATE"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$i'.'$DOMAIN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP_ADDRESS'"
+            }]
+        }
+        }]
+    }
+        '
+
 done
