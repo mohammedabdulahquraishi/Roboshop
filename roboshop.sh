2a4b5a460cffb305993e8a0ce5873780626efe90
Mohammed Abdulah Quraishi <mdabdulahquraish@gmail.com>
2023-12-21 10:54:06 +0530

    creating instances automatically

--- roboshop.sh
+++ roboshop.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
-
-AMI=ami-03265a0778a880afb
+AMI_ID=ami-03265a0778a880afb
 SG_ID=sg-07062d9c12192a838
+INSTANCES=("MONGODB" "CART" "CATALOGUE" "MYSQL" "PAYMENT" "RABBITMQ" "REDIS" "SHIPPING" "USER" "WEB")
 ZONE_ID=Z02773932U4NB4GX343HA
-INSTANCE=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-DOMAIN_NAME="littlesimba.online"
+DOMAIN_NAME=littlesimba.online
 
-for i in "{$INSTANCE[@]}"
+for i in  "${INSTANCES[@]}"
 do
-    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
-    then
-    INSTANCE_TYPE="t3.small"
+    echo "Instance is: $i"
+    if [ $i == "MONGODB" ] || [ $i == "MYSQL" ] || [ $i == "SHIPPING" ]
+    then 
+        INSTANCE_TYPE="t3.small"
     else
-    INSTANCE_TYPE="t2.micro"
+        INSTANCE_TYPE="t2.micro"
     fi
-IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-07062d9c12192a838b3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
-aws route53 change-resource-record-sets \
+
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-07062d9c12192a838 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+
+    echo "$i: $IP_ADDRESS"
+
+    aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
     --change-batch '
     {
@@ -32,6 +36,5 @@ aws route53 change-resource-record-sets \
         }
         }]
     }
-        '
-
-done
+    '
+done   
